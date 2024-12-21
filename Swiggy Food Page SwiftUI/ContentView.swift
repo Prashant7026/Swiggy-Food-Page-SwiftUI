7a6c7b59e08c6eaeac1e6dbb390f6144de2d8d78
Prashant3-playo <prashant@playo.co>
2024-12-21 12:02:48 +0530

    first commit

---
+++ Swiggy Food Page SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swiggy Food Page SwiftUI
+//
+//  Created by Prashant Kumar Soni on 21/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swiggy Food Page SwiftUI/Swiggy_Food_Page_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swiggy_Food_Page_SwiftUIApp.swift
+//  Swiggy Food Page SwiftUI
+//
+//  Created by Prashant Kumar Soni on 21/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Swiggy_Food_Page_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
